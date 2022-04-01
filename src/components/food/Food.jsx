68e5260e1ae3b2306e748c857fac646a76f3f9d1
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-04-01 08:26:28 +0300

    api testing

--- src/components/food/Food.jsx
+++ src/components/food/Food.jsx
@@ -1,28 +1,22 @@
 import React from "react";
 
 const Food = (props) => {
-
   return (
     <>
       <div className="ui raised very padded text container segment">
-        <div className="ui card">
+        <div className="ui centered card">
           <div className="image">
             <img src="/images/avatar2/large/kristy.png" alt="" />
           </div>
           <div className="content">
-            <p className="header">HI{props.dish}</p>
-            <div className="meta">
-              <span className="date">Joined in 2013</span>
-            </div>
-            <div className="description">
-              Kristy is an art director living in New York.
-            </div>
+            <p className="header">{props.dish}</p>
+            <div className="meta"></div>
+            <div className="description">{props.desc}</div>
           </div>
           <div className="extra content">
-            <a>
-              <i className="user icon"></i>
-              22 Friends
-            </a>
+            <p>
+              {props.ing} | <b>{props.mes}</b>
+            </p>
           </div>
         </div>
       </div>
--- src/components/foods/Foods.jsx
+++ src/components/foods/Foods.jsx
@@ -2,21 +2,23 @@ import React, { useState, useEffect } from "react";
 import Food from "../food/Food";
 
 const Foods = () => {
-
   const [dishes, setDishes] = useState([]);
 
   useEffect(() => {
     fetch("https://random-data-api.com/api/food/random_food?size=15")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .then((data) => setDishes(data))
+      .then((data) => setDishes(data));
   }, []);
 
   return (
     <>
       {dishes.map((dish) => (
         <Food
-        dish={dish.id}
+          key={dish.id}
+          dish={dish.dish}
+          desc={dish.description}
+          ing={dish.ingredient}
+          mes={dish.measurement}
         />
       ))}
     </>
